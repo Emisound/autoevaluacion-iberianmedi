2b9ef616a1bd8a5c62f877606739187bed1d8962
Emisound <68460212+Emisound@users.noreply.github.com>
2025-07-29 13:36:15 +0200

    Create Dashboard.jsx

---
+++ src/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Dashboard de Administrador</h1>
+      <p>Pronto verás aquí todas las evaluaciones.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
